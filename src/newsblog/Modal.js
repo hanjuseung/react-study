5448adb4e77a5dc4c5d19e484c9de2ab9ed3c4ab
hanjuseung <rhdqn0501@naver.com>
2025-05-23 12:08:46 +0900

    map 반복처리, props 전달(변수,함수)

--- src/newsblog/Modal.js
+++ src/newsblog/Modal.js
@@ -1,12 +1,23 @@
-
+import {useState} from 'react';
 
 function Modal(props){
+    //props = {title:'', likeCount:10, post:[], setPost:setPost함수
+    //         bgColor:'lightcyan'}
 
+    let [num,setNum] = useState(10);
     return(
-        <div className='modal'>
-            <h3>{props.title}</h3>
+        <div className='modal' style={{backgroundColor:props.bgColor}}>
+            <h3>{props.title} 🤍{props.likeCount}</h3>
             <p>날짜</p>
             <p>상세내용</p>
+
+            <button onClick={()=>{
+                //이 버튼을 눌렀을때
+                //천번째 제목 오늘의 뉴스 -> Today News
+                let temp = [...props.post];
+                temp[0] = 'Today News';
+                props.setPost(temp);
+            }}>제목변경</button>
         </div>
     );
 }
--- src/newsblog/NewsBlog.js
+++ src/newsblog/NewsBlog.js
@@ -2,88 +2,71 @@ import './NewsBlog.css';
 import { useState } from 'react';
 import Modal from './Modal';
 
-function NewsBlog() {
+function NewsBlog(){
 
-    let title = 'React study';
+    let title = "React Study";
 
-    let [post, setPost] = useState(['오늘의 뉴스', '어제의 뉴스', '내일의 뉴스']);
-    //                            ['Today News','어제의 뉴스','내일의 뉴스'] 
+    let [news1, setNews1] = useState('오늘의 뉴스');
+    let [news2, setNews2] = useState('어제의 뉴스');
+    let [news3, setNews3] = useState('내일의 뉴스');
 
-    let [count, setCount] = useState([0,0,0]);
+    let [news, setNews] = useState(['오늘의 뉴스', '어제의 뉴스', '내일의 뉴스']);
+                //                 ['Today News', '어제의 뉴스', '내일의 뉴스']
+
+    let [likeCount1, setLikeCount1] = useState(0);
+    let [likeCount2, setLikeCount2] = useState(0);
+    let [likeCount3, setLikeCount3] = useState(0);
+
+    let [likeCountArr, setLikeCountArr] = useState( [0, 0, 0] );
 
     let [showModal, setShowModal] = useState(false);
+    //    0  1 
 
     let [selectedTitle, setSelectedTitle] = useState('');
+    let [selectedLikeCount, setSelectedLikeCount] = useState(0);
+
     return (
         <div>
             <div className='black-nav'>
                 <h4>Blog Header</h4>
-                <p style={{ color: 'orange', fontSize: '20px' }}>{title}</p>
-            </div>
-
-            <div className='post-list'>
-                <h4 onClick={()=>{
-                    if(showModal){
-                        setShowModal(false);}
-                    else {
-                        setShowModal(true);}
-
-                    setSelectedTitle(post[0]);
-                }}>{post[0]}<span onClick={() => { 
-                    let count2 = [...count];
-                    count2[0]++;
-                    setCount(count2) }}>🤍</span>{count[0]}</h4>
-                <p>내용 무</p>
-            </div>
-
-            <div className='post-list'>
-                <h4 onClick={()=>{
-                    if(showModal){
-                        setShowModal(false);}
-                    else {
-                        setShowModal(true);}
-                        setSelectedTitle(post[1]);
-                }}>{post[1]}<span onClick={() => { 
-                    let count2 = [...count];
-                    count2[1]++;
-                    setCount(count2) }}>🤍</span>{count[1]}</h4>
-                <p>내용 무</p>
-            </div>
-
-            <div className='post-list'>
-                <h4 onClick={()=>{
-                    if(showModal){
-                        setShowModal(false);}
-                    else {
-                        setShowModal(true);}
-                        setSelectedTitle(post[2]);
-                }}>{post[2]}<span onClick={(event) => { 
-                    event.stopPropagation(); //전파를 막겠다
-                    // 속해있는 부모 요소의 이벤트 처리까지 전파되는걸 막겠다.
-                    //<h4><span></span></h4>
-                    let count2 = [...count];
-                    count2[2]++;
-                    setCount(count2) }}>🤍</span>{count[2]}</h4>
-                <p>내용 무</p>
+                <p style={ {color:"orange", fontSize:'20px'} }>{title}</p>
             </div>
 
             {
-                //showModal == true ? <Modal/> : null
-                showModal && <Modal title={selectedTitle}/>
+                news.map((item, index)=>{   // 0 1 2 
+                    //return <div className='post-list'>
+                    return (
+                        <div className='post-list'>
+                            <h4 onClick={()=>{
+                                setShowModal(!showModal);
+                                setSelectedTitle(item);
+                                setSelectedLikeCount(likeCountArr[index]);
+                            }}>{item} <span onClick={(event)=>{
+                                
+                                event.stopPropagation();
+                                
+                                let temp = [...likeCountArr];
+                                temp[index] += 1;
+                                setLikeCountArr(temp);
+
+                            }}>❤</span>{likeCountArr[index]}</h4>
+                            <p>내용 무</p>
+                        </div>
+                    )
+
+                })
             }
 
-
-            {/* <button onClick={() => {
-                let post2 = [...post];
-                post2[0] = 'Today News';
-                post2[1] = 'Yesterday News';
-                post2[2] = 'Tomorrow News';
-                setPost(post2);
-            }}>제목 변경</button> */}
-
-
+            {
+                //showModal == true ? <Modal/> : null
+                //props 전달 
+                // 변수, 배열, 객체, 함수, 요소(컴포넌트)
+                showModal && <Modal title={selectedTitle} likeCount={selectedLikeCount} 
+                                    news={news} setNews={setNews}
+                                    bgColor='lightblue'/>
+            }
+            
         </div>
-
     );
 }
 
