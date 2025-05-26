d292e03305ab9d8be0bca88bb40b7f27a65d601a
hanjuseung <rhdqn0501@naver.com>
2025-05-26 11:43:50 +0900

    연습5 + 옵션 선택 삭제 기능 추가

--- src/practice/practice05/ColorBox.js
+++ src/practice/practice05/ColorBox.js
@@ -1,6 +1,12 @@
 function ColorBox(props){
 
-    return(<div className="box" style={{ backgroundColor:props.bgColor }}></div>);
+    return(<div className="box" style={{ backgroundColor:props.bgColor }}>
+        <button onClick={()=>{
+            let box2 = [...props.box];
+            box2.splice(props.index,1);
+            props.setBox(box2);
+        }}>x</button>
+    </div>);
     
 }
 
--- src/practice/practice05/practice05.js
+++ src/practice/practice05/practice05.js
@@ -2,6 +2,64 @@ import ColorBox from './ColorBox';
 import './practice05.css';
 import { useState } from 'react';
 
+function Practice05() {
+    let [box, setBox] = useState(['red', 'blue', 'green', 'black']);
+    return (
+
+        <div>
+            <button onClick={() => {
+                let box2 = [...box];
+                box2.unshift('red');
+                setBox(box2);
+            }}>앞빨간박스추가</button>
+            <button onClick={() => {
+                let box2 = [...box];
+                box2.unshift('blue');
+                setBox(box2);
+            }}>앞파란박스추가</button>
+            <button onClick={() => {
+                let box2 = [...box];
+                box2.unshift('green');
+                setBox(box2);
+            }}>앞초록박스추가</button>
+            <button onClick={() => {
+                let box2 = [...box];
+                box2.push('red');
+                setBox(box2);
+            }}>뒤빨간박스추가</button>
+            <button onClick={() => {
+                let box2 = [...box];
+                box2.push('blue');
+                setBox(box2);
+            }}>뒤파란박스추가</button>
+            <button onClick={() => {
+                let box2 = [...box];
+                box2.push('green');
+                setBox(box2);
+            }}>뒤초록박스추가</button>
+            <button onClick={() => {
+                let box2 = [...box];
+                box2.shift();
+                setBox(box2);
+            }}>앞 박스 삭제</button>
+            <button onClick={() => {
+                let box2 = [...box];
+                box2.pop();
+                setBox(box2);
+            }}>뒤 박스 삭제</button>
+            <br>
+            </br>
+            {
+                box.map((item, index) => {
+                    return (
+                        <ColorBox box={box} setBox={setBox} bgColor={item} index={index}/>
+                    );
+                })
+            }
+        </div>
+    );
+}
+/*
 function Practice05(){
     let [box, setBox] = useState(['red', 'blue','green','black']);
     return (
@@ -49,10 +107,10 @@ function Practice05(){
         </div>
     );
 }
-
+*/
 /*
 function Practice05() {
-    let [box, setBox] = useState([<Boxred />, <Boxblue />, <Boxgreen />, <Boxblack />]);
+    let [box, setBox] = useState(['red','blue','green','black']);
     return (
 
         <div>
@@ -103,25 +161,48 @@ function Practice05() {
     );
     function Boxred() {
         return (
-            <div className="box" style={{ backgroundColor: 'red' }}></div>
+            <div className="box" style={{ backgroundColor: 'red' }}>
+                <button onClick={() => {
+                    let box2 = [...box];
+                    box2.splice(box2[index], 1);
+                    setBox(box2);
+                }}>x</button>
+            </div>
         );
     }
     function Boxblue() {
         return (
-            <div className="box" style={{ backgroundColor: 'blue' }}></div>
+            <div className="box" style={{ backgroundColor: 'blue' }}>
+                <button onClick={() => {
+                    let box2 = [...box];
+                    box2.splice(box2[index], 1);
+                    setBox(box2);
+                }}>x</button>
+            </div>
         );
     }
     function Boxgreen() {
         return (
-            <div className="box" style={{ backgroundColor: 'green' }}></div>
+            <div className="box" style={{ backgroundColor: 'green' }}>
+                <button onClick={() => {
+                    let box2 = [...box];
+                    box2.splice(box2[index], 1);
+                    setBox(box2);
+                }}>x</button>
+            </div>
         );
     }
     function Boxblack() {
         return (
-            <div className="box" style={{ backgroundColor: 'black' }}></div>
+            <div className="box" style={{ backgroundColor: 'black' }}>
+                <button onClick={() => {
+                    let box2 = [...box];
+                    box2.splice(box2[index], 1);
+                    setBox(box2);
+                }}>x</button>
+            </div>
         );
     }
 }
-    */
-
+*/
 export default Practice05;
