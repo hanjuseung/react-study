49fc9ff6640993995ab57bd1dae9addcd551cb11
hanjuseung <rhdqn0501@naver.com>
2025-05-26 10:38:52 +0900

    연습5 동적인 컴포넌트 처리

---
+++ src/practice/practice05/ColorBox.js
@@ -0,0 +1,7 @@
+function ColorBox(props){
+
+    return(<div className="box" style={{ backgroundColor:props.bgColor }}></div>);
+    
+}
+
+export default ColorBox;
---
+++ src/practice/practice05/practice05.js
@@ -0,0 +1,127 @@
+import ColorBox from './ColorBox';
+import './practice05.css';
+import { useState } from 'react';
+
+function Practice05(){
+    let [box, setBox] = useState(['red', 'blue','green','black']);
+    return (
+<div>
+            <button onClick={() => {
+                let box2 = ['red',...box];
+                setBox(box2);
+            }}>앞빨간박스추가</button>
+            <button onClick={() => {
+                let box2 = ['blue',...box];
+                setBox(box2);
+            }}>앞파란박스추가</button>
+            <button onClick={() => {
+                let box2 = ['green',...box];
+                setBox(box2);
+            }}>앞초록박스추가</button>
+            <button onClick={() => {
+                let box2 = [...box,'red'];
+                setBox(box2);
+            }}>뒤빨간박스추가</button>
+            <button onClick={() => {
+                let box2 = [...box,'blue'];
+                setBox(box2);
+            }}>뒤파란박스추가</button>
+            <button onClick={() => {
+                let box2 = [...box,'green'];
+                setBox(box2);
+            }}>뒤초록박스추가</button>
+            <button onClick={() => {
+                let box2 = [...box];
+                box2.shift();
+                setBox(box2);
+            }}>앞 박스 삭제</button>
+            <button onClick={() => {
+                let box2 = [...box];
+                box2.pop();
+                setBox(box2);
+            }}>뒤 박스 삭제</button>
+            <br>
+            </br>
+            {box.map((item)=>{
+                // return <div className="box" style={{ backgroundColor:item }}></div>
+                return <ColorBox bgColor={item}/>
+            })}
+        </div>
+    );
+}
+
+/*
+function Practice05() {
+    let [box, setBox] = useState([<Boxred />, <Boxblue />, <Boxgreen />, <Boxblack />]);
+    return (
+
+        <div>
+            <button onClick={() => {
+                let box2 = [...box];
+                box2.unshift(<Boxred />);
+                setBox(box2);
+            }}>앞빨간박스추가</button>
+            <button onClick={() => {
+                let box2 = [...box];
+                box2.unshift(<Boxblue />);
+                setBox(box2);
+            }}>앞파란박스추가</button>
+            <button onClick={() => {
+                let box2 = [...box];
+                box2.unshift(<Boxgreen />);
+                setBox(box2);
+            }}>앞초록박스추가</button>
+            <button onClick={() => {
+                let box2 = [...box];
+                box2.push(<Boxred />);
+                setBox(box2);
+            }}>뒤빨간박스추가</button>
+            <button onClick={() => {
+                let box2 = [...box];
+                box2.push(<Boxblue />);
+                setBox(box2);
+            }}>뒤파란박스추가</button>
+            <button onClick={() => {
+                let box2 = [...box];
+                box2.push(<Boxgreen />);
+                setBox(box2);
+            }}>뒤초록박스추가</button>
+            <button onClick={() => {
+                let box2 = [...box];
+                box2.shift();
+                setBox(box2);
+            }}>앞 박스 삭제</button>
+            <button onClick={() => {
+                let box2 = [...box];
+                box2.pop();
+                setBox(box2);
+            }}>뒤 박스 삭제</button>
+            <br>
+            </br>
+            {box}
+        </div>
+    );
+    function Boxred() {
+        return (
+            <div className="box" style={{ backgroundColor: 'red' }}></div>
+        );
+    }
+    function Boxblue() {
+        return (
+            <div className="box" style={{ backgroundColor: 'blue' }}></div>
+        );
+    }
+    function Boxgreen() {
+        return (
+            <div className="box" style={{ backgroundColor: 'green' }}></div>
+        );
+    }
+    function Boxblack() {
+        return (
+            <div className="box" style={{ backgroundColor: 'black' }}></div>
+        );
+    }
+}
+    */
+
+export default Practice05;
