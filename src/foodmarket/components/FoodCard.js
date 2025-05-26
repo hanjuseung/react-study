6ff320a2ed992b3d9e46a24d963843263ab313d9
hanjuseung <rhdqn0501@naver.com>
2025-05-26 17:20:18 +0900

    URL경로 파라미터 전달(useParams), 상세페이지 표시 및 상세페이지 링크 이동

--- src/foodmarket/components/FoodCard.js
+++ src/foodmarket/components/FoodCard.js
@@ -1,23 +1,26 @@
-import {Card, Button, Col } from 'react-bootstrap';
+import { Card, Button, Col } from 'react-bootstrap';
+import { useNavigate } from 'react-router';
 
 function FoodCard(props) {
+    let navigate = useNavigate();
+
     return (
-        <>
-                <Card style={{ width: '18rem' }}>
-                    {/* <Card.Img variant="top" src={food1} /> */}
-                    <Card.Img variant="top" src={process.env.PUBLIC_URL + props.foods.imgPath}/>
-                    <Card.Body>
-                        <Card.Title>{props.foods.title}</Card.Title>
-                        <Card.Text>
-                            {props.foods.content}
-                        </Card.Text>
-                        <Card.Text>
-                            {props.foods.price}
-                        </Card.Text>
-                        <Button variant="primary">Go somewhere</Button>
-                    </Card.Body>
-                </Card>
-        </>
+        <Card style={{ width: '18rem' }}>
+            {/* <Card.Img variant="top" src={food1} /> */}
+            <Card.Img variant="top" src={process.env.PUBLIC_URL + props.foods.imgPath} />
+            <Card.Body>
+                <Card.Title>{props.foods.title}</Card.Title>
+                <Card.Text>
+                    {props.foods.content}
+                </Card.Text>
+                <Card.Text>
+                    {props.foods.price}
+                </Card.Text>
+                <Button variant="primary" onClick={()=>{
+                    navigate("/detail/"+ props.foods.id);
+                }}>상세보기</Button>
+            </Card.Body>
+        </Card>
     );
 }
 
