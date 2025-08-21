d90af8b54d379d343f1aa740aa06829cbc1b5f28
hanjuseung <rhdqn0501@naver.com>
2025-08-21 16:47:49 +0900

    [React Spring] react 로그인 API 요청, 세션 로그인 처리

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import Practice04 from './practice/practice04/practice04';
 import FoodMarket from './foodmarket/pages/FoodMarket';
 import Practice06 from './practice/Practice06/Practice06';
 import AppRS from './react_spring/AppRS';
+import Login from './react_spring/Login';
 
 function App() {
 
@@ -31,7 +32,8 @@ function App() {
   // return <Practice04/>;
   // return <FoodMarket/>;
   // return <Practice06/>;
-  return <AppRS/>;
+  // return <AppRS/>;
+  return <Login/>;
 
   /*
   return (
